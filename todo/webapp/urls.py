af70a30c5212b30688c729d7c246faf4b100a7ad
ilef rajhi <itsilef9gmail.com>
2024-07-14 23:10:39 +0100

    fixed the complete view and adding styles to main

--- todo/webapp/urls.py
+++ todo/webapp/urls.py
@@ -2,17 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name = "login"),
-    path('register/', views.register, name = "register"),
-    path('reset_password/', views.reset_password, name = "reset_password"),
-    path('logout/', views.logout, name = "logout"),
-    path('main/', views.main, name = "main"),
+    path('', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('reset_password/', views.reset_password, name='reset_password'),
+    path('logout/', views.logout, name='logout'),
+    path('main/', views.main, name='main'),
     path('add/', views.add_task, name='add_task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
     path('add_user/', views.add_user, name='add_user'),
     path('update/<int:task_id>/', views.update_task, name='update_task'),
     path('incomplete_tasks/', views.incomplete_tasks, name='incomplete_tasks'),
-    path('completed_tasks/', views.completed_tasks, name='completed_tasks'),
+    path('complete_task/<int:task_id>/', views.complete_task, name='complete_task'),  # Include <int:task_id> here
     path('search/', views.search_tasks, name='search_tasks'),
-    path('login_user/', views.login_user, name='login_user'), 
+    path('login_user/', views.login_user, name='login_user'),
 ]
